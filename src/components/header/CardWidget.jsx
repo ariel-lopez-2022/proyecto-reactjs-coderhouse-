aea0401fe75e4774ed5fbf6f76b235ec143cf8c1
lopez ariel <saltaportal210@gmail.com>
2022-09-02 16:33:04 -0300

    se agrego boton contador

--- src/components/header/CardWidget.jsx
+++ src/components/header/CardWidget.jsx
@@ -4,7 +4,7 @@ import logoCarrito from "../../assets/images/logoCarrito.png";
 const Carrito =()=>{ 
   return (
     <button type="button" className="btn position-relative">
-       <a href="!#"><img src={logoCarrito} width="38"/></a>
+       <a href="!#"><img src={logoCarrito} width="38" alt="logo carrito"/></a>
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0
           </span>
     </button>
