bfef143bb38cec5195cdaa7e097847879d22d1bd
lopez ariel <saltaportal210@gmail.com>
2022-09-08 20:05:41 -0300

    se modificaron estilos

--- src/assets/helpers/programacion.js
+++ src/assets/helpers/programacion.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 const programacion = [{id:1, descripcion:"Aprende Logica de Programacion", imagen:"../../images/programacion/logicadeprogramacion.jpg", precio:1500, cantidad:10},
 {id:2, descripcion:"Aprende Logica de Programacion", imagen:"../../images/programacion/aprendelogica.jpg", precio:2500, cantidad:10},
 {id:3, descripcion:"Programacion para Principiantes", imagen:"../../images/programacion/programacionprincipiantes.jpg", precio:1000, cantidad:10},
@@ -7,14 +5,5 @@ const programacion = [{id:1, descripcion:"Aprende Logica de Programacion", image
 {id:5, descripcion:"Aprende JavaScript Principiante", imagen:"../../images/programacion/javascript.jpg", precio:2500, cantidad:10},
 {id:6, descripcion:"Programacion Python Principiante", imagen:"../../images/programacion/pythonprincipiante.jpg", precio:3500, cantidad:10}
 ]
-=======
-const programacion = [{id:1, descripcion:"Logica de Programacion", imagen:"../../images/programacion/logicadeprogramacion.jpg", precio:1500, cantidad:10},
-      {id:2, descripcion:"Aprende Logica de Programacion", imagen:"../../images/programacion/aprendelogica.jpg", precio:2500, cantidad:10},
-      {id:3, descripcion:"Programacion para Principiantes", imagen:"../../images/programacion/programacionprincipiantes.jpg", precio:1000, cantidad:10},
-      {id:4, descripcion:"Aprende C#", imagen:"../../images/programacion/aprendec.jpg", precio:3500, cantidad:10},
-      {id:5, descripcion:"Aprende JavaScript Principiante", imagen:"../../images/programacion/javascript.jpg", precio:2500, cantidad:10},
-      {id:6, descripcion:"Programacion Python Principiante", imagen:"../../images/programacion/pythonprincipiante.jpg", precio:3500, cantidad:10}
-     ]
->>>>>>> 83317d9c068d7cd3179a22639fb7e6415c3a8609
 
 export default programacion;   
